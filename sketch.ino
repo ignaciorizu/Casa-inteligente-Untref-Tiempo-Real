a5c2d292efc98085f38de106f614da503ad32798
federicoweberok <weber.federic@gmail.com>
2025-11-13 12:40:11 -0300

    Se agrega sistema de iluminación extendido para la entrada y el pasillo

--- sketch.ino
+++ sketch.ino
@@ -5,13 +5,17 @@
 #include <freertos/queue.h>
 
 // ----- CONFIG ----- //
-#define DHTPIN 2       // temperatura
-#define LDR_PIN 32     // luxometro -- 100 >=
-#define LED_LIGHT 12   // iluminación --
-#define LED_THERM 27   // termostato
-#define PIR_PIN 19     // movimiento --
-#define LED_ALARM 25   // alarma / sirena --
-#define SPEAKER_PIN 26  // sonido de alarma
+#define LDR_PIN_ENTRADA 32     // Sensor de Luz - Entrada
+#define LDR_PIN_PASILLO 35     // Sensor de Luz - Pasillo
+
+#define LED_LIGHT_ENTRADA 12   // Iluminación - Entrada
+#define LED_LIGHT_PASILLO 14   // Iluminación - Pasillo
+
+#define DHTPIN 2        // Sensor de Temperatura
+#define PIR_PIN 19      // Sensor de Movimiento
+#define LED_THERM 27    // Calefaccion
+#define LED_ALARM 25    // Alarma
+#define SPEAKER_PIN 26  // Sirena de alarma
 
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
@@ -23,16 +27,19 @@ QueueHandle_t motionQueue;
 // ----- Tareas ----- //
 void TaskTemp(void *pv);
 void TaskLight(void *pv);
-void TaskThermostat(void *pv);
 void TaskMotion(void *pv);
+void TaskThermostat(void *pv);
 void TaskAlarm(void *pv);
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(LDR_PIN, INPUT);
+  pinMode(LDR_PIN_ENTRADA, INPUT);
+  pinMode(LDR_PIN_PASILLO, INPUT);
+  pinMode(LED_LIGHT_ENTRADA, OUTPUT);
+  pinMode(LED_LIGHT_PASILLO, OUTPUT);
+  
   pinMode(PIR_PIN, INPUT);
-  pinMode(LED_LIGHT, OUTPUT);
   pinMode(LED_THERM, OUTPUT);
   pinMode(LED_ALARM, OUTPUT);
   pinMode(SPEAKER_PIN, OUTPUT);
@@ -44,8 +51,8 @@ void setup() {
 
   xTaskCreate(TaskTemp, "Temp", 4096, NULL, 3, NULL);
   xTaskCreate(TaskLight, "Light", 2048, NULL, 2, NULL);
-  xTaskCreate(TaskThermostat, "Thermostat", 4096, NULL, 2, NULL);
   xTaskCreate(TaskMotion, "Motion", 2048, NULL, 3, NULL);
+  xTaskCreate(TaskThermostat, "Thermostat", 4096, NULL, 2, NULL);
   xTaskCreate(TaskAlarm, "Alarm", 2048, NULL, 2, NULL);
 }
 
@@ -65,20 +72,15 @@ void TaskTemp(void *pv) {
 }
 
 void TaskLight(void *pv) {
-  while (1) {
-    int light = digitalRead(LDR_PIN);
-    digitalWrite(LED_LIGHT, light);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
+  const int ldrPins[2] = {LDR_PIN_ENTRADA, LDR_PIN_PASILLO};
+  const int ledPins[2] = {LED_LIGHT_ENTRADA, LED_LIGHT_PASILLO};
 
-void TaskThermostat(void *pv) {
-  float temp;
   while (1) {
-    if (xQueueReceive(tempQueue, &temp, portMAX_DELAY)) {
-      if (temp < 20) digitalWrite(LED_THERM, HIGH);
-      else if (temp > 24) digitalWrite(LED_THERM, LOW);
+    for (int i = 0; i < 2; i++) {
+      int light = digitalRead(ldrPins[i]);
+      digitalWrite(ledPins[i], light);
     }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -89,6 +91,16 @@ void TaskMotion(void *pv) {
       xQueueSend(motionQueue, &alert, 0);
     }
     vTaskDelay(300 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskThermostat(void *pv) {
+  float temp;
+  while (1) {
+    if (xQueueReceive(tempQueue, &temp, portMAX_DELAY)) {
+      if (temp < 20) digitalWrite(LED_THERM, HIGH);
+      else if (temp > 24) digitalWrite(LED_THERM, LOW);
+    }
   }
 }
 
@@ -105,3 +117,4 @@ void TaskAlarm(void *pv) {
     }
   }
 }
+
